463ddaf388dc04b10d63a603a4b90af7e16822d1
Oles Malysh <olesmalish37@gmail.com>
2025-01-12 20:28:21 +0100

    Create sealed interface 'Route'

--- app/src/main/java/com/example/habitstracker/navigation/RoutesMainScreen.kt
+++ app/src/main/java/com/example/habitstracker/navigation/RoutesMainScreen.kt
@@ -1,13 +1,46 @@
 package com.example.habitstracker.navigation
 
+import kotlinx.serialization.Serializable
+
 sealed class RoutesMainScreen(
     val route: String,
 ) {
+
     object Main : RoutesMainScreen(route = "MainScreen")
+
     object AddHabit : RoutesMainScreen(route = "AddHabitScreen")
     object CreateNewHabit : RoutesMainScreen(route = "CreateOwnHabitScreen")
     object RepeatPicker : RoutesMainScreen(route = "RepeatPickerScreen")
     object EditHabit: RoutesMainScreen(route = "EditHabitScreen")
     object EditRepeatPicker : RoutesMainScreen(route = "EditRepeatPickerScreen")
+
+}
+
+sealed interface Route {
+
+    @Serializable
+    data object Today: Route
+
+    @Serializable
+    data object History: Route
+
+    @Serializable
+    data object Me: Route
+
+    @Serializable
+    data object AddHabit: Route
+
+    @Serializable
+    data class CreateHabit(val param: String): Route
+
+    @Serializable
+    data object RepeatPicker: Route
+
+    @Serializable
+    data class EditHabit(val id: Int): Route
+
+    @Serializable
+    data class EditRepeatPicker(val id: Int): Route
+
 
 }
