ec17245b3c625ac58acc0a7c0aae87e9f566c0a0
Oles Malysh <olesmalish37@gmail.com>
2025-11-18 20:32:26 +0100

    test commit to proof github notification

--- app/src/main/java/com/example/habitstracker/me/presentation/MeScreen.kt
+++ app/src/main/java/com/example/habitstracker/me/presentation/MeScreen.kt
@@ -98,6 +98,7 @@ fun MeScreenRoot(signInViewModel: SignInViewModel, syncViewModel: SyncViewModel)
         }
     }*/
 
+
     MeScreen(
         user = signInState.userData,
         message = signInState.signInError,
